61546a2599ab5de224cd2e1438c6a4b38ddfa7c6
elliottcable <me@ell.io>
2009-12-30 14:03:28 -0900

    Screw lib/ and bin/. It’s time to branch, isn’t it?

--- Executables/Paws.js
+++ Executables/Paws.js
@@ -1,9 +1,10 @@
 #!/usr/bin/env node
 
 // Absolutes; `from` still ain’t complete, and packaging doesn’t work as of
-// this writing.
+// this writing. This should become `from.package('poopy.js')` and
+// `from.package('Paws.js')`.
 var poopy = from.absolute('/Users/elliottcable/Code/poopy.js/lib/poopy.js').wait(),
-     paws = from.relative('../lib/Paws.js').wait();
+     paws = from.relative('../Packages/Paws.js/Paws.js').wait();
 
 // OMGDEBUGGING
 // from.absolute('/Users/elliottcable/Code/probe/lib/probe.js').wait().probe(paws);
