61804ae2b06aa7e443e245a842ddf971debc84f2
elliottcable <me@ell.io>
2009-12-30 12:48:40 -0900

    Infrastructure to iterate words…

--- bin/Paws.js
+++ bin/Paws.js
@@ -10,30 +10,30 @@ var poopy = from.absolute('/Users/elliottcable/Code/poopy.js/lib/poopy.js').wait
 
 // Normally, we’d cat a file here, and hand it to `paws.compile`. Instead,
 // we’re going to jump directly to interpreting a routine root:
-// var ast = ['routine', ['statement',
-//   ['word', 'foo'], ['word', ' '],
-//   ['word', 'bar'], ['word', ' '],
-//   ['word', 'baz'], ['word', ' '],
-//   ['word', 'qux'], ['word', ' '],
-//   ['word', 'quux'], ['word', ' '],
-//   ['word', 'corge'], ['word', ' '],
-//   ['word', 'grault'], ['word', ' '],
-//   ['call', ['statement',
-//     ['word', 'garply'], ['word', ' '],
-//     ['word', 'waldo'], ['word', ' '],
-//     ['word', ','], ['word', ' '],
-//     ['word', 'fred'], ['word', ' '],
-//     ['word', 'plugh']
-//   ]], ['word', ' '],
-//   ['word', 'xyzzy'], ['word', ' '],
-//   ['word', 'thud']
+// var ast = ['routine', ['statement'
+// , ['word', 'foo']
+// , ['word', 'bar']
+// , ['word', 'baz']
+// , ['word', 'qux']
+// , ['word', 'quux']
+// , ['word', 'corge']
+// , ['word', 'grault']
+// , ['call', ['statement'
+//   , ['word', 'garply']
+//   , ['word', 'waldo']
+//   , ['word', ',']
+//   , ['word', 'fred']
+//   , ['word', 'plugh']
+//   ]]
+// , ['word', 'xyzzy']
+// , ['word', 'thud']
 // ]];
 
 // Too complex for now! Let’s go simpler:
-var ast = ['routine', ['statement',
-  ['word', 'foo'], ['word', ' '],
-  ['word', 'bar'], ['word', ' '],
-  ['word', 'baz'], ['word', ' ']
+var ast = ['routine', ['statement'
+, ['word', 'foo']
+, ['word', 'bar']
+, ['word', 'baz']
 ]];
 
 var rootRoutine = paws.routine.beget({ body : ast });
