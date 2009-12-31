3427cd040e1825dedd7714dbda17c1425f817561
elliottcable <me@ell.io>
2009-12-31 01:00:16 -0900

    Ugh! Need to deal with this, conceptually.

--- Packages/Paws.js/Paws.js
+++ Packages/Paws.js/Paws.js
@@ -3,6 +3,11 @@ var poopy = from.absolute('/Users/elliottcable/Code/poopy.js/lib/poopy.js').wait
 return (function(){ var paws;
   paws = new(Object);
   
+  // FIXME: Huge, annoying problem. Since inheritance is implemented libspace,
+  //        none of the primatives will provide inheritance. That is,
+  //        `infrastructure tuple` won’t properly inherit from `infrastructure
+  //        list`, and so on and so forth.
+  
         paws.list = from.relative('list.js').export({ paws : paws }).wait();
        paws.tuple = from.relative('tuple.js').export({ paws : paws }).wait();
       paws.string = from.relative('string.js').export({ paws : paws }).wait();
