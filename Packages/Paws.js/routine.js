74f45939e854d4279dce10f60b6efcba91383fdb
elliottcable <me@ell.io>
2010-01-01 20:23:23 -0900

    Hiding `body` on `routine`.

--- Packages/Paws.js/routine.js
+++ Packages/Paws.js/routine.js
@@ -1,24 +1,54 @@
 return (function(){ var routine;
   routine = paws.list.beget();
   
+  routine.errors = {
+    invalidBinding: new(Error)("Routines may only be bound to `list`s"),
+    invalidArgument: new(Error)("Routines may only be called with a `list`"),
+    invalidBody: new(Error)("Routines’ body must be a native `Function`, " +
+                            "or an abstracted syntax tree")
+  };
+  
   // Constructs a `paws.routine`, given a routine blueprint. If a `nate`
   // element is available on the blueprint, that will be used as the
   // `routine.nate`. The same is true of `body`.
-  routine.constructor = function (blueprint) {
+  routine.constructor = function (blueprint) { var that;
     paws.list.constructor.apply(this, arguments);
     
-    if        (typeof blueprint      !== 'undefined') {
-      if      (typeof blueprint.nate !== 'undefined') {
-          this.nate = blueprint.nate }
-      else if (typeof blueprint.body !== 'undefined') {
-          this.body = blueprint.body }
-      else {
-          this.body = ['routine', []] } };
+    that = this;
+    (function(){ var body, binding;
+      // These are private methods. Do not use them; use their public-API
+      // equivalents instead.
+      that._binding = function () { return binding };
+      that._bind = function (list) { 
+        if (!paws.list.isPrototypeOf(list)) {
+          throw(routine.errors.invalidBinding) };
+        binding = list;
+      };
+      
+      that._setBody = function (val) {
+        if (!val instanceof Array && typeof val !== 'function') {
+          throw(routine.errors.invalidBody) }
+        body = val;
+      };
+      that._call = function (argument) {
+        if (!paws.list.isPrototypeOf(argument)) {
+          throw(routine.errors.invalidArgument) };
+        
+        if (typeof body === 'function') {
+          body.apply(binding, [argument, that]) }
+        else { /* FIXME: What? */ };
+      };
+      
+    })();
+    
+    this._bind(paws['null']);
+    
+    if (typeof blueprint      !== 'undefined' &&
+        typeof blueprint.body !== 'undefined') { this._setBody(blueprint.body) }
+                                          else { this._setBody([]) };
     
     // FIXME: This should be defined *on the list*. Not on the JS `Object`.
     this.scope = paws.scope.beget();
-    // FIXME: This should be private
-    this.binding = paws['null'];
   };
   
   // Executes a routine; either farming out the (native) implementation, or
@@ -30,11 +60,7 @@ return (function(){ var routine;
   // it.
   //-
   // TODO: Automatically handle native-returns from native routines
-  routine.call = function (argument) {
-    if (typeof this.nate !== 'undefined') {
-      this.nate.apply(this.binding, [argument, this]) }
-    else { this.interpret(argument) }
-  };
+  routine.call = function (argument) { this._call(argument) };
   
   return routine;
 })()
