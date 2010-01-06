3983ea8da4b9bc463a84125bb9c35c8acbf68131
elliottcable <me@ell.io>
2010-01-05 20:12:12 -0900

    Revert "The `paws.definition` constructor now takes a ‘hash’"
    This reverts commit 9b4ee192d650da1d45c7eac26313db932ba44a1b.
    
    Conflicts:
    
    	Packages/Paws.js/Things/definition.js

--- Packages/Paws.js/Things/definition.js
+++ Packages/Paws.js/Things/definition.js
@@ -13,33 +13,20 @@ return (function(){ var definition, rootDefinition;
                                  "optional metadata list")
   };
   
-  definition.constructor = function (blueprint) { var keys = [], values = [], me;
-    me = arguments.callee;
-    
+  definition.constructor = function (blueprint) {
     // Hell, definitions are really just tuples that are a bit stricter about
     // their content
     if ( typeof blueprint         !== 'undefined' &&
          typeof blueprint.content !== 'undefined' ) {
-      if (blueprint.content instanceof Array) {
-        
-        if (!paws.string.isPrototypeOf(blueprint.content[0]) &&
-             paws.string !== blueprint.content[0]) {
-          throw(definition.errors.invalidName) };
-        if (blueprint.content.length > 3 || blueprint.content.length < 2) {
-          throw(definition.errors.invalidStructure) };
-        
-        if (blueprint.content.length === 2) {
-          blueprint.content.push(paws.list.beget()) };
-        
-      } else {
-        
-        for(key in blueprint.content) {
-          keys.unshift(key); values.unshift(blueprint.content[key]) };
-        blueprint.content = [keys[0], values[0]];
-        
-        me.apply(this, arguments); return;
-        
-      } };
+      if (!paws.string.isPrototypeOf(blueprint.content[0]) &&
+          !paws.string === blueprint.content[0]) {
+        throw(definition.errors.invalidName) };
+      if (blueprint.content.length > 3 || blueprint.content.length < 2) {
+        throw(definition.errors.invalidStructure) };
+      
+      if (blueprint.content.length < 3) {
+        blueprint.content.push(paws.list.beget()) };
+    };
     
     paws.tuple.constructor.apply(this, arguments);
   };
