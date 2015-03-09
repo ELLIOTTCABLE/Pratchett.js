001679e60597e07dc3b90199dc1e165aa87e0b9c
elliottcable <me@ell.io>
2015-03-09 17:16:42 -0500

    + adding debugging and usage to test.sh

--- Scripts/test.sh
+++ Scripts/test.sh
@@ -1,8 +1,13 @@
 #!/usr/bin/env sh
-set -e
-
+                                                                                      set +o verbose
 # Usage:
 # ------
+# This script runs both the unit-test suite and, if they've been downloaded, checks conformance with
+# the Paws rulebooks.
+# 
+#    npm test
+#    RUN_LETTERS=yes npm test    # To execute the Letters, as well
+puts() { printf %s\\n "$*" ;}
 
 test_env="$npm_package_config_test_ENV"
 test_files="$npm_package_config_test_files"
@@ -19,8 +24,20 @@ if [ -n "${RESPECT_TRACING##[NFnf]*}" ]; then
    verbose='4'          # 'warning' and worse
 fi
 
+# FIXME: This should really support comma-seperated DEBUG values, as per `node-debug`:
+#        https://github.com/visionmedia/debug
+[ "$DEBUG" = 'Paws.js:scripts' ] && DEBUG_SCRIPTS=0
+[ -n "$DEBUG_SCRIPTS" ] && puts "Script debugging enabled (in: `basename $0`)." >&2
+[ -n "$DEBUG_SCRIPTS" ] && VERBOSE="${VERBOSE:-7}"
+
+[ -n "$DEBUG_SCRIPTS" ] && puts \
+   "Requested hook:        '$1'"                               \
+   "Default hooks:         '$npm_package_config_git_hooks'"    \
+   "" >&2
+
 
 [ -z "${SILENT##[NFnf]*}${QUIET##[NFnf]*}" ] && [ "${VERBOSE:-4}" -gt 6 ] && set -x
+set -e
 
 env TRACE_REACTOR="$trace_reactor" VERBOSE="$verbose" \
     NODE_ENV="$test_env" ./node_modules/.bin/mocha    \
@@ -46,4 +63,9 @@ if [ -n ${RUN_LETTERS+x} ]; then
       "$PWD/$npm_package_config_dirs_rulebook/The Letters/"*   \
       $TAPER_FLAGS -- $CHECK_FLAGS $RULEBOOK_FLAGS
    
-fi ; fi
+fi
+
+else
+   puts 'Clone the rulebook from this URL to `./'$npm_package_config_dirs_rulebook'` to check Rulebook compliance:'
+   puts '   <https://github.com/Paws/Rulebook.git>'
+fi
