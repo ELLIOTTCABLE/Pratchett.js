3fd080849090e9aa61b7b71af57621fa84caa2cd
elliottcable <me@ell.io>
2015-03-09 18:27:10 -0500

    + further improving test.sh's debugging output

--- Scripts/test.sh
+++ Scripts/test.sh
@@ -7,65 +7,69 @@
 # 
 #    npm test
 #    RUN_LETTERS=yes npm test    # To execute the Letters, as well
-puts() { printf %s\\n "$*" ;}
+puts() { printf %s\\n "$@" ;}
+pute() { printf %s\\n "~~ $*" >&2 ;}
 
 test_env="$npm_package_config_test_ENV"
 test_files="$npm_package_config_test_files"
 mocha_ui="$npm_package_config_mocha_ui"
 mocha_reporter="$npm_package_config_mocha_reporter"
 
-if [ "$PRE_COMMIT" = "true" ]; then
+# FIXME: This should support *excluded* modules with a minus, as per `node-debug`:
+#        https://github.com/visionmedia/debug
+echo "$DEBUG" | grep -qE '(^|,\s*)(\*|Paws.js(:(scripts|\*))?)($|,)' && DEBUG_SCRIPTS=0
+[ -n "$DEBUG_SCRIPTS" ] && pute "Script debugging enabled (in: `basename $0`)."
+[ -n "$DEBUG_SCRIPTS" ] && VERBOSE="${VERBOSE:-7}"
+
+if [ -n "${PRE_COMMIT##[NFnf]*}" ]; then
+   [ -n "$DEBUG_SCRIPTS" ] && pute "Enabling pre-commit mode."
    mocha_reporter='dot'
    RESPECT_TRACING='no'
 fi
 
-if [ -n "${RESPECT_TRACING##[NFnf]*}" ]; then
-   trace_reactor='no'
-   verbose='4'          # 'warning' and worse
+if [ -n "${RESPECT_TRACING##[YTyt]*}" ]; then
+   [ -n "$DEBUG_SCRIPTS" ] && pute "Disrespecting tracing flags"
+   VERBOSE='4'          # 'warning' and worse
+   unset TRACE_REACTOR
 fi
 
-# FIXME: This should really support comma-seperated DEBUG values, as per `node-debug`:
-#        https://github.com/visionmedia/debug
-[ "$DEBUG" = 'Paws.js:scripts' ] && DEBUG_SCRIPTS=0
-[ -n "$DEBUG_SCRIPTS" ] && puts "Script debugging enabled (in: `basename $0`)." >&2
-[ -n "$DEBUG_SCRIPTS" ] && VERBOSE="${VERBOSE:-7}"
-
 [ -n "$DEBUG_SCRIPTS" ] && puts \
-   "Requested hook:        '$1'"                               \
-   "Default hooks:         '$npm_package_config_git_hooks'"    \
+   "Rulebook directory:    '$npm_package_config_dirs_rulebook'"   \
+   "Pre-commit mode:       ${PRE_COMMIT:--}"                      \
+   "Verbosity:             '$VERBOSE'"                            \
+   "Tracing reactor:       ${TRACE_REACTOR:+Yes!}"                \
+   "Running letters:       ${RUN_LETTERS:--}"                     \
    "" >&2
 
 
 [ -z "${SILENT##[NFnf]*}${QUIET##[NFnf]*}" ] && [ "${VERBOSE:-4}" -gt 6 ] && set -x
 set -e
 
-env TRACE_REACTOR="$trace_reactor" VERBOSE="$verbose" \
-    NODE_ENV="$test_env" ./node_modules/.bin/mocha    \
-   --compilers coffee:coffee-script/register          \
-   --reporter "$mocha_reporter" --ui "$mocha_ui"      \
+env NODE_ENV="$test_env" ./node_modules/.bin/mocha      \
+   --compilers coffee:coffee-script/register            \
+   --reporter "$mocha_reporter" --ui "$mocha_ui"        \
    $MOCHA_FLAGS "$test_files"
 
 if [ -d "$PWD/$npm_package_config_dirs_rulebook" ]; then
-   env TRACE_REACTOR="$trace_reactor" VERBOSE="$verbose"       \
-       NODE_ENV="$test_env" ./node_modules/.bin/taper          \
+   env NODE_ENV="$test_env" ./node_modules/.bin/taper          \
       --runner "$PWD/Executables/paws.js"                      \
       --runner-param='check'                                   \
       "$PWD/$npm_package_config_dirs_rulebook/The Ladder/"*    \
       "$PWD/$npm_package_config_dirs_rulebook/The Gauntlet/"*  \
       $TAPER_FLAGS -- $CHECK_FLAGS
    
-if [ -n ${RUN_LETTERS+x} ]; then
-   env TRACE_REACTOR="$trace_reactor" VERBOSE="$verbose"       \
-       NODE_ENV="$test_env" ./node_modules/.bin/taper          \
+if [ -n "${RUN_LETTERS##[NFnf]*}" ]; then
+   env NODE_ENV="$test_env" ./node_modules/.bin/taper          \
       --runner "$PWD/Executables/paws.js"                      \
       --runner-param='check'                                   \
       --runner-param='--expose-specification'                  \
       "$PWD/$npm_package_config_dirs_rulebook/The Letters/"*   \
       $TAPER_FLAGS -- $CHECK_FLAGS $RULEBOOK_FLAGS
-   
 fi
 
 else
+   [ -n "$DEBUG_SCRIPTS" ] && pute "Rulebook directory not found."
+
    puts 'Clone the rulebook from this URL to `./'$npm_package_config_dirs_rulebook'` to check Rulebook compliance:'
    puts '   <https://github.com/Paws/Rulebook.git>'
 fi
