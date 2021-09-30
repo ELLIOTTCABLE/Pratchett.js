808ece16690b69266a59046faa10e75e5d6d8c18
ELLIOTTCABLE <git@ell.io>
2021-09-30 17:15:38 -0500

    + addtl partialSecond()

--- Source/nodash/function/partial.ts
+++ Source/nodash/function/partial.ts
@@ -1,56 +1,88 @@
 /**
- * Close over `parameter`, prepending it to any arguments passed to `f`.
- *
- * This is a bit like a poorman's `curry()`:
- *
- * ```ts
- * const multiply = (a, b) => a * b
- * const doubler = partial(multiply, 2)
- *
- * multiply(6, 2) //=> 12
- * ```
- *
- * @param f The function to wrap
- * @typeParam First the type of `f`'s first parameter
- * @param parameter A value to prepend to any arguments used in subsequent calls
- * @typeParam Rest a tuple-type for the rest of `f`'s parameters
- * @typeParam Result the return-type of `f`
- * @return The wrapped version of `f` that will prepend the given argument
+ * ### Some supporting type-level functionality
  */
-export function partial<First, Rest extends unknown[], Result>(
-   f: (this: void, first: First, ...rest: Rest) => Result,
-   parameter: First,
-): (...parameters: Rest) => Result
+
+type headOf<TupleType extends unknown[]> = TupleType extends [unknown, ...unknown[]]
+   ? TupleType[0]
+   : never
+
+type tailOf<TupleType extends unknown[]> = ((..._a: TupleType) => unknown) extends (
+   _head: never,
+   ...tail: infer TailType
+) => unknown
+   ? TailType
+   : []
+
+type hasTail<T extends unknown[]> = T extends [] | [unknown] ? false : true
+
+type firstParameterOf<FunctionType extends (..._a: never[]) => unknown> =
+   FunctionType extends (..._a: infer Parameters) => unknown ? headOf<Parameters> : never
+
+type secondParameterOf<FunctionType extends (..._a: never[]) => unknown> =
+   FunctionType extends (..._a: infer Parameters) => unknown
+      ? headOf<tailOf<Parameters>>
+      : never
+
+type secondAndSubseqParametersOf<FunctionType extends (..._a: never[]) => unknown> =
+   FunctionType extends (..._a: infer Parameters) => unknown ? tailOf<Parameters> : never
+
+type thirdAndSubseqParametersOf<FunctionType extends (..._a: never[]) => unknown> =
+   FunctionType extends (..._a: infer Parameters) => unknown
+      ? tailOf<tailOf<Parameters>>
+      : never
+
+type hasTailParameters<FunctionType extends (..._a: never[]) => unknown> =
+   FunctionType extends (..._a: infer Parameters) => unknown ? hasTail<Parameters> : never
+
+type hasThirdAndTailParameters<FunctionType extends (..._a: never[]) => unknown> =
+   FunctionType extends (..._a: infer Parameters) => unknown
+      ? hasTail<tailOf<Parameters>>
+      : never
 
 /**
- * Close over `parameter`, prepending it to any arguments passed to `f`.
- *
- * This is a bit like a poorman's `curry()`:
- *
- * ```ts
- * const multiply = (a, b) => a * b
- * const doubler = partial(multiply, 2)
- *
- * multiply(6, 2) //=> 12
- * ```
- *
- * @param f The function to wrap
- * @typeParam First the type of `f`'s first parameter
- * @param parameter A value to prepend to any arguments used in subsequent calls
- * @typeParam Rest a tuple-type for the rest of `f`'s parameters
- * @typeParam Result the return-type of `f`
- * @return The wrapped version of `f` that will prepend the given argument
+ * ### The meat
  */
-export function partial<This, First, Rest extends unknown[], Result>(
-   f: (this: This, first: First, ...rest: Rest) => Result,
-   parameter: First,
-): (this: This, ...parameters: Rest) => Result
 
-export function partial<This, First, Rest extends unknown[], Result>(
-   f: (this: This, first: First, ...rest: Rest) => Result,
-   parameter: First,
-): (this: This, ...parameters: Rest) => Result {
-   return function (this: This, ...rest: Rest) {
-      return f.call(this, parameter, ...rest)
+/*
+ * The return-type of `partial()`, this ...
+ * DOCME:
+ */
+type PartialApplicationResult<FunctionType extends (..._a: never[]) => unknown> =
+   hasTailParameters<FunctionType> extends true
+      ? (...rest: secondAndSubseqParametersOf<FunctionType>) => ReturnType<FunctionType>
+      : () => ReturnType<FunctionType>
+
+export function partial<FunctionType extends (..._a: never[]) => unknown>(
+   f: FunctionType,
+   parameter: firstParameterOf<FunctionType>,
+): PartialApplicationResult<FunctionType> {
+   return function (
+      this: ThisParameterType<FunctionType>,
+      ...rest: secondAndSubseqParametersOf<FunctionType>
+   ) {
+      // eslint-disable-next-line @typescript-eslint/no-unsafe-return -- no idea why this breaks
+      return f.call(this, parameter, ...rest) as ReturnType<FunctionType>
+   }
+}
+
+type PartialSecondApplicationResult<FunctionType extends (..._a: never[]) => unknown> =
+   hasThirdAndTailParameters<FunctionType> extends true
+      ? (
+           first: firstParameterOf<FunctionType>,
+           ...rest: thirdAndSubseqParametersOf<FunctionType>
+        ) => ReturnType<FunctionType>
+      : (first: firstParameterOf<FunctionType>) => ReturnType<FunctionType>
+
+export function partialSecond<FunctionType extends (..._a: never[]) => unknown>(
+   f: FunctionType,
+   parameter: secondParameterOf<FunctionType>,
+): PartialSecondApplicationResult<FunctionType> {
+   return function (
+      this: ThisParameterType<FunctionType>,
+      first: firstParameterOf<FunctionType>,
+      ...rest: thirdAndSubseqParametersOf<FunctionType>
+   ) {
+      // eslint-disable-next-line @typescript-eslint/no-unsafe-return -- no idea why this breaks
+      return f.call(this, first, parameter, ...rest) as ReturnType<FunctionType>
    }
 }
